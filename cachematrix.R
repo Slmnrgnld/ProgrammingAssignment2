9d387029320e9e363a67fefbd0c416eb81c9fc0c
Slmnrgnld <87270506+Slmnrgnld@users.noreply.github.com>
2021-07-12 09:31:38 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,42 @@
-makeCacheMatrix <- function(x = matrix()) { ##this defines the argument with a default mode of "matrix"
-  a <- NULL	                                ## a is initialized as NULL which will hold the value of the matrix inverse
-  set <- function(y){                       ## this defines that the  function is assigned to 'set'
-  x <<- y                                   ## this is the value of matrix which is in the parent environment
-  a <<- NULL                                ##if a new matrix is present, this resets 'a' to NULL
-  }
-  get <- function()x                        ##this defines the value of the function and returns the matrix arguments value
+# the following are a pair of function that can be used to cache the inverse of a matrix
+# the following are based on the Example: Caching the Mean of a Vector
 
-  setInverse <- function(inverse) a <<- inverse           ## this assigns value of 'a' in parent environment
-  getInverse <- function() a                              ## this gets the value of 'j'
-  list(set = set, get = get,                              ## this is in referance to the functions with the $ operator
-  setInverse = setInverse, 
-  getInverse = getInverse)
+
+## 'z' is inputted as a variable and is set as the name representator for matrix
+
+makeCacheMatrix <- function(z = matrix()) { 
+  a <- NULL	          
+  
+  set <- function(y){                       
+  z <<- y                                   
+  a <<- NULL                                
+  }
+  
+  get <- function()z                        
+
+  setInv <- function(Inv) z <<- Inverse          
+  getInv <- function() z                              
+  list(set = set, get = get,                             
+  setInv = setInv, 
+  getInv = getInv)
 }
 
-##Please include your own comment to explain your code (Required in Rubric)
 
-cacheSolve <- function(x, ...) {
-## Return a matrix that is the inverse of 'x'
-  a <- x$getInverse()
-  if(!is.null(a)){
-  message("getting cached data")
+## the following lines is used to obtain the inverse value
+
+cacheSolve <- function(z,...) {
+
+  z <- x$getInv()
+  
+  if(!is.null(z)){
+  message("getting the inversed matrix")
   return(a)
   }
-  mat <- x$get()
-  a <- solve(mat,...)
+  
+  data <- x$get()
+  
+  z <- solve(data,...)
   x$setInverse(a)
+  
   a
 }
